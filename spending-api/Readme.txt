14aa719252ed0567b8230f320848301c8274a7d2
BrandonRPL <153643841+BrandonRPL@users.noreply.github.com>
2024-06-17 13:03:37 -0500

    back

---
+++ spending-api/Readme.txt
@@ -0,0 +1,3 @@
+crear controlador con ejemplo de api
+
+php artisan make:controller (nombre del controlador) --api
---
+++ spending-api/routes/api.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Http\Controllers\ExpenseController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::apiResource('expenses',ExpenseController::class);
